184a311a12dc26fabd63470c0779f533bc8a65ed
antoniareynal <reynala@udesa.edu.ar>
2022-09-13 10:59:04 -0300

    funciona favoritos

--- mi-proyecto/src/Pages/Favoritos.js
+++ mi-proyecto/src/Pages/Favoritos.js
@@ -2,33 +2,44 @@ import React, {Component} from 'react';
 import CardMovie from '../Componentes/CardMovie/CardMovie'
 
 class Favoritos extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
+            props: props,
             peliculasFav: []
         }
     }
-    componenDidMount() {
+    componentDidMount() {
         
-        let favoritos = [];
-        let recuperoStorage = localStorage.getItem('favoritos')
+        let favoritosToArray;
+        let recuperoStorage = localStorage.getItem('favoritos');
 
+        
         if (recuperoStorage !== null) {
-            favoritos = JSON.parse(recuperoStorage)
-            console.log('favoritos');
-            let peliculasFavoritas = []
-            favoritos.forEach(id => {
+            favoritosToArray = JSON.parse(recuperoStorage)
+            let peliculasFavoritas = [];
+            /* favoritos.forEach(id => {
                 fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=1caaa22005845643c0863fd9677bc21c&language=en-US`)
                     .then(response => response.json())
-                    .then(data => peliculasFavoritas.push(data))
+                    .then(data => peliculasFavoritas.push(data.results))
                     .then(()=> this.setState(
                         {peliculasFav: peliculasFavoritas}
                     ))
-                    console.log('peliculasFav')
                     .catch(error => console.log(error))
-                    
-            })}
-    };
+            })} */
+            for(let i = 0; i < favoritosToArray.length; i++){
+                fetch(`https://api.themoviedb.org/3/movie/${favoritosToArray[i]}?api_key=1caaa22005845643c0863fd9677bc21c&language=en-US`)
+                    .then(response => response.json())
+                    .then(data => {
+                        peliculasFavoritas.push(data);
+                        this.setState(
+                            {peliculasFav: peliculasFavoritas}
+                        )
+                    })
+                    .catch(error => console.log(error))
+            }
+        };
+    }
 
     agregarYQuitarDeFavoritos(id){
         let favoritos = [];
@@ -55,26 +66,21 @@ class Favoritos extends Component {
         let favoritosToString = JSON.stringify(favoritos);
         localStorage.setItem('favoritos', favoritosToString);
 
-        console.log(favoritos);
     }
+
 
     render() {
-        console.log(this.state.peliculasFav)
-            return ( 
+        console.log(this.state.storage)
+        return(
             <React.Fragment>
-                <h1 className = "tituloPrincipal" > This are your favorite movies: </h1> 
-                
-                <section className = "peliculasSeries peliculasFav resultadosBusqueda" > 
+                <h1 className = "tituloPrincipal" > This are your favorite movies: </h1>
+                <section className = "peliculasSeries peliculasFav resultadosBusqueda" >
                     {this.state.peliculasFav.map((oneMovie, idx) => < CardMovie key = {oneMovie + idx} movieData = {oneMovie}/>)} 
                 </section> 
-            
-                
             </React.Fragment>
-
-                )
-                   
-            }
-
+        )
     }
+
+}
 
 export default Favoritos;

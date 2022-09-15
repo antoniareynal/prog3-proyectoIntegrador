8a74e2159a20c1f0cf1b36619742f21e5c8d0cf6
Agustín Ramírez Calvo <aramirezcalvo@udesa.edu.ar>
2022-09-15 14:52:46 -0300

    Merge

--- mi-proyecto/src/Pages/PopularMovies.js
+++ mi-proyecto/src/Pages/PopularMovies.js
@@ -8,7 +8,7 @@ class PopularMovies extends Component {
         this.state = {
             page: 1,
             pMovies: [], // Todas las pelis
-            
+            morepMovies: []
             
         }
     }
@@ -19,6 +19,7 @@ class PopularMovies extends Component {
             .then(data => this.setState(
                 {
                 pMovies: data.results,
+                morepMovies: data.results,
                 page: this.state.page + 1
             }
             )).catch(error => console.log(error));
@@ -60,7 +61,7 @@ render(){
         <h1 className="tituloPrincipal">Popular Movies</h1>
         
         <section className="peliculasSeries peliculasP">
-                {this.state.pMovies.map((oneMovie, idx) => <CardMovie key ={ oneMovie + idx} movieData = {oneMovie}/>)}
+                {this.state.morepMovies.map((oneMovie, idx) => <CardMovie key ={ oneMovie + idx} movieData = {oneMovie}/>)}
         </section>
         <button className="boton-vermas-home"onClick={()=> this.verMas()}>Load more</button>
 
